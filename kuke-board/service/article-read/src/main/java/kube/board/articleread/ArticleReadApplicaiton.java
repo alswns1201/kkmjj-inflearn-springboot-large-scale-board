4a76b36ae7d89d555598ac7b44ac7e91bb9396f7
kkmjj <alswns1201@gmail.com>
2026-03-21 21:45:12 +0900

    init project files

---
+++ kuke-board/service/article-read/src/main/java/kube/board/articleread/ArticleReadApplicaiton.java
@@ -0,0 +1,9 @@
+package kube.board.articleread;
+
+import org.springframework.boot.SpringApplication;
+
+public class ArticleReadApplicaiton {
+    public static void main(String[] args){
+        SpringApplication.run(ArticleReadApplicaiton.class,args);
+    }
+}
---
+++ kuke-board/service/hot-article/src/main/java/kube/board/hotarticle/HotarticleApplication.java
@@ -0,0 +1,11 @@
+package kube.board.hotarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotarticleApplication {
+    public static void main(String[] args){
+        SpringApplication.run(HotarticleApplication.class,args);
+    }
+}
