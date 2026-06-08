e28927d3db73ed4c2602da7e591416139b3af105
jun <alswns1201@gmail.com>
2026-06-08 09:51:40 +0900

    view 어뷰징 방지 lock repository

--- kuke-board/service/view/src/main/java/kube/board/view/service/ArticleViewService.java
+++ kuke-board/service/view/src/main/java/kube/board/view/service/ArticleViewService.java
@@ -2,17 +2,28 @@ package kube.board.view.service;
 
 
 import kube.board.view.repository.ArticleViewCountRepository;
+import kube.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackupProcessor articleViewCountBackupProcessor;
     private static final int BACK_UP_BACH_SIZE = 100;
 
+    private static final Duration TTL = Duration.ofMinutes(10);
+
     public Long increase(Long articleId,Long userId){
+
+        if(articleViewDistributedLockRepository.lock(articleId,userId,TTL)){
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count =  articleViewCountRepository.increase(articleId);
         if(count == BACK_UP_BACH_SIZE){
             articleViewCountBackupProcessor.backup(articleId,count);
